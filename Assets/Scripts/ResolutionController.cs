255d7268c5272bee326749705896cab7c4897038
Shuu <ghwls328@naver.com>
2020-11-08 07:54:34 +0900

    Initial resolution forcer
    Force Aspect ratio basic set

---
+++ Assets/Scripts/ResolutionController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionController : MonoBehaviour
+{
+    float ratio;
+    int width;
+    int height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = 1600;
+        height = 900;
+        Screen.SetResolution(width, height, false);
+        
+        ratio = (float) width / (float) height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.fullScreen == false)
+        {
+            int curWidth = Screen.width;
+            int curHeight = Screen.height;
+            
+            if (width != curWidth || height != curHeight)
+            {
+                float curRatio = (float)curWidth / (float)curHeight;
+                if (curRatio > ratio)
+                {
+                    curHeight = (int)(curWidth / ratio);
+                }
+                else if(curRatio < ratio)
+                {
+                    curWidth = (int)(curHeight * ratio);
+                }
+                Screen.SetResolution(curWidth, curHeight, false);
+                width = curWidth;
+                height = curHeight;
+            }
+        }                
+    }
+}
